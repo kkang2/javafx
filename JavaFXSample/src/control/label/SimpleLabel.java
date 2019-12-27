7b28f8610fc106807ef269333f42c2e03e8482bc
kkang2 <mallakkang@gmail.com>
2019-12-27 11:44:18 +0900

    - 컨트롤 예제 추가

---
+++ JavaFXSample/src/control/label/SimpleLabel.java
@@ -0,0 +1,23 @@
+package control.label;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class SimpleLabel extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("SimpleStage App!!");
+		
+		Label label = new Label("Hello World, JavaFX !");
+		Scene scene = new Scene(label, 400, 200);
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
